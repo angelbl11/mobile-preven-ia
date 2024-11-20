399f11bf872384b296cdf0852ba7f723cdb0e5ec
abarajas11 <abarajas@yofio.co>
2024-11-19 21:44:52 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # mobile-preven-ia
-# mobile-preven-ia
---
+++ android/app/src/main/kotlin/com/example/mobile_preven_ia_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobile_preven_ia_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
